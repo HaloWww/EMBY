69d3814cd46d4675e3a0cedd7d00f49ff6174ef3
HaloWww <64211024+HaloWww@users.noreply.github.com>
2022-06-11 16:01:48 +0800

    Update emby.sh

--- emby.sh
+++ emby.sh
@@ -6,11 +6,17 @@ mkdir /home/cloud1-zm
 mkdir /home/cloud2-zm
 mkdir /home/cloud3-zm
 mkdir /home/mix1-xg
+#创建rclone配置目录
+mkdir -p /root/.config/rclone
 #添加rclone开机自启
-wget 
-wget
-wget
-wget
+wget https://raw.githubusercontent.com/HaloWww/EMBY/main/rclone/cloud1-zm
+wget https://raw.githubusercontent.com/HaloWww/EMBY/main/rclone/cloud2-zm
+wget https://raw.githubusercontent.com/HaloWww/EMBY/main/rclone/cloud3-zm
+wget https://raw.githubusercontent.com/HaloWww/EMBY/main/rclone/mix1-xg
+mv ./cloud1-zm /etc/init.d/cloud1-zm
+mv ./cloud2-zm /etc/init.d/cloud2-zm
+mv ./cloud3-zm /etc/init.d/cloud3-zm
+mv ./mix1-xg /etc/init.d/mix1-xg
 chmod +x /etc/init.d/cloud1-zm
 chmod +x /etc/init.d/cloud2-zm
 chmod +x /etc/init.d/cloud3-zm
@@ -19,10 +25,13 @@ update-rc.d -f cloud1-zm defaults
 update-rc.d -f cloud2-zm defaults
 update-rc.d -f cloud3-zm defaults
 update-rc.d -f mix1-xg defaults
+
+#安装emby
+wget https://github.com/MediaBrowser/Emby.Releases/releases/download/4.7.1.0/emby-server-deb_4.7.1.0_amd64.deb
+dpkg -i emby-server-deb_4.7.1.0_amd64.deb
+
+#安装完成之后启动服务
 bash /etc/init.d/cloud1-zm start
 bash /etc/init.d/cloud2-zm start
 bash /etc/init.d/cloud3-zm start
 bash /etc/init.d/mix1-xg start
-#安装emby
-wget https://github.com/MediaBrowser/Emby.Releases/releases/download/4.7.1.0/emby-server-deb_4.7.1.0_amd64.deb
-dpkg -i emby-server-deb_4.7.1.0_amd64.deb
