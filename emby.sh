1031211be72fea36d49ba2e09ed81fdbb44eb46f
HaloWww <64211024+HaloWww@users.noreply.github.com>
2023-05-28 01:46:32 +0800

    Update emby.sh

--- emby.sh
+++ emby.sh
@@ -40,6 +40,48 @@ mkdir /home/auto9-wa
 mkdir /home/auto10-wa
 mkdir /home/mix1-xg
 mkdir /home/mix2-xg
+mkdir /home/mix3-xg
+mkdir /home/mix4-xg
+
+mkdir -p /home/cache/cloud1-zm
+mkdir -p /home/cache/cloud2-zm
+mkdir -p /home/cache/cloud3-zm
+mkdir -p /home/cache/cloud4-zm
+mkdir -p /home/cache/cloud5-zm
+mkdir -p /home/cache/cloud6-zm
+mkdir -p /home/cache/cloud7-zm
+mkdir -p /home/cache/cloud8-zm
+mkdir -p /home/cache/cloud9-zm
+mkdir -p /home/cache/cloud10-zm
+mkdir -p /home/cache/cloud11-zm
+mkdir -p /home/cache/cloud12-zm
+mkdir -p /home/cache/cloud13-zm
+mkdir -p /home/cache/cloud14-zm
+mkdir -p /home/cache/cloud15-zm
+mkdir -p /home/cache/cloud16-wa
+mkdir -p /home/cache/cloud17-wa
+mkdir -p /home/cache/cloud18-wa
+mkdir -p /home/cache/cloud19-wa
+mkdir -p /home/cache/cloud20-wa
+mkdir -p /home/cache/cloud21-wa
+mkdir -p /home/cache/cloud22-wa
+mkdir -p /home/cache/cloud23-wa
+mkdir -p /home/cache/cloud24-wa
+mkdir -p /home/cache/cloud25-wa
+mkdir -p /home/cache/auto1-wa
+mkdir -p /home/cache/auto2-wa
+mkdir -p /home/cache/auto3-wa
+mkdir -p /home/cache/auto4-wa
+mkdir -p /home/cache/auto5-wa
+mkdir -p /home/cache/auto6-wa
+mkdir -p /home/cache/auto7-wa
+mkdir -p /home/cache/auto8-wa
+mkdir -p /home/cache/auto9-wa
+mkdir -p /home/cache/auto10-wa
+mkdir -p /home/cache/mix1-xg
+mkdir -p /home/cache/mix2-xg
+mkdir -p /home/cache/mix3-xg
+mkdir -p /home/cache/mix4-xg
 
 #创建rclone配置目录
 mkdir -p /root/.config/rclone
